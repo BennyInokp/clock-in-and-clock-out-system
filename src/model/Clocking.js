6563ef4c6ed47f9b85bd49358e872ad13bd223b3
BennyInokp <inokpuareakhilome@gmail.com>
2023-09-05 09:44:51 +0100

    clock-in and clock-out system

--- src/model/Clocking.js
+++ src/model/Clocking.js
@@ -16,58 +16,9 @@ const clockingSchema = new mongoose.Schema({
     enum: ['in', 'out'],
     required: true,
   },
-  // Other log-related fields.
+  
 });
 
  export default mongoose.model("Clocking", clockingSchema);
 
-// import Sequelize  from "sequelize";
-// import db from "../config/sequelize.js";
 
-// const Checking = db.define('checking', {
-//   id: {
-//     type: Sequelize.DataTypes.INTEGER,
-//     primaryKey: true,
-//     autoIncrement: true,
-//   },
-//   client_id: {
-//     type: Sequelize.DataTypes.INTEGER,
-//     allowNull: false,
-//   },
-//   checkinTime: {
-//     type: Sequelize.DataTypes.DATE,
-//     allowNull: false,
-//   },
-//   checkoutTime: {
-//     type: Sequelize.DataTypes.DATE,
-//   },
-// });
-
-// // export default Checking;
-
-// const clockingSchema = new mongoose.Schema({
-//   id: {
-//     type: String,
-//     required: true,
-//   },
-
-//   employee: {
-//         type: String,
-//         ref: 'Employee',
-//        required: true,
-//       },
-  
-//   clockinTime: {
-//     type: Date,
-//     required: true,
-//   },
-//   clockoutTime: {
-//     type: Date,
-//   },
-// });
-
-// // const Clocking = mongoose.model("Clocking", ClockingSchema);
-
-// // export default Clocking;
-
-// export default mongoose.model("Clocking", clockingSchema);
