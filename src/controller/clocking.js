fca64c9e4a68a3a22da878f601ae88f82b24d00e
BennyInokp <inokpuareakhilome@gmail.com>
2023-09-05 22:04:14 +0100

    clock-in and clock-out-system

--- src/controller/clocking.js
+++ src/controller/clocking.js
@@ -1,39 +1,3 @@
-// import Clocking from "../model/Clocking.js";
-
-//   export const clockin = async (req, res) => {
-//     
-//     try {
-//       const { employee_id } = req.body;
-  
-//       const existingClockin = await Clocking.findById({
-//         employee_id,
-//         clockingTime :{
-    
-//           $gte: new Date(new Date().setHours(0, 0, 0, 0)),
-//         },
-//       });
-
-//     if (existingClockin) {
-//       return res
-//         .status(400)
-//         .json({ message: "Employee has already clocked in for today" });
-//     }
-//     const newClockin = await Clocking.create({
-//       employee_id,
-//       clockInTime: new Date(),
-//     });
-
-//     res
-//       .status(201)
-//       .json({ message: "Clock-in successful", clockin: newClockin });
-//   } catch (error) {
-//     console.error(error);
-//     res
-//       .status(500)
-//       .json({ message: "Failed to clock in", error: error.message });
-//   }
-// };
-
 import Clocking from '../model/Clocking.js';
 import Employee from '../model/Employee.js'; // Import the Employee model
 
@@ -71,45 +35,41 @@ if (!existingEmployee) {
   }
 };
 
-
-export const clockout = async (req, res ) => {
+    
+export const clockout = async (req, res) => {
   try {
     const { employee_id } = req.body;
 
     // Check if the provided employee_id exists in the Employee collection
     const existingEmployee = await Employee.findById(employee_id);
-if (!existingEmployee) {
+    if (!existingEmployee) {
       return res.status(400).json({ message: 'Employee not found' });
     }
-    
-// Check if the employee has already clocked out today
-    const existingClockout = await Clocking.findById({
-        employee: employee_id,
-        timestamp: {
-          $gte: new Date(new Date().setHours(0, 0, 0, 0)),
-        },
-    
-    });
 
-    if (existingClockout) {
-      return res
-        .status(400)
-        .json({ message: "Employee has  or already clocked out" });
+    // Find the last clock-in entry for the employee for today
+    const lastClockin = await Clocking.findOne({
+      employee: employee_id,
+      timestamp: {
+        $gte: new Date(new Date().setHours(0, 0, 0, 0)),
+      },
+      type: 'in', // Assuming 'in' is for clock-in and 'out' is for clock-out
+    }).sort({ timestamp: -1 }); // Sort by timestamp in descending order to get the latest entry
+
+    if (!lastClockin) {
+      return res.status(400).json({ message: 'Employee has not clocked in today' });
     }
-    lastClockin.clockoutTime = new Date();
+
+    // Update the last clock-in entry to record the clock-out time
+    lastClockin.type = 'out',
+    lastClockin.timestamp = new Date();
     await lastClockin.save();
 
     res.status(200).json({
-      message: "Clock-out successful",
-      clockoutTime: lastClockin.clockoutTime,
+      message: 'Clock-out successful',
+      clockoutTime: lastClockin.timestamp,
     });
   } catch (error) {
     console.error(error);
-    res
-      .status(500)
-      .json({ message: "Failed to clock out", error: error.message });
+    res.status(500).json({ message: 'Failed to clock out', error: error.message });
   }
 };
-
-    
-
